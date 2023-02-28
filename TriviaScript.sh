7c09deae522c771bd32b0252b29e8bb471aefee5
MAReyes617 <95144350+MAReyes617@users.noreply.github.com>
2023-02-28 14:27:29 -0500

    question1

--- TriviaScript.sh
+++ TriviaScript.sh
@@ -5,3 +5,10 @@ sleep 1
 echo 'Before we get started, What is the contestants name?!'
 read name
 echo ' Welcome to the show $name, Now lets get started!'
+sleep 1 
+echo 'Question #1, What does the pwd command do ?
+A) Nothing 
+B) Show Location
+C) Shows Password
+D) Moves Within Directory '
+Read 
