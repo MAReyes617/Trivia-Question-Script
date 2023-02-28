3a85d037a9830ec2eff54b250c7839dc84b87682
MAReyes617 <95144350+MAReyes617@users.noreply.github.com>
2023-02-28 14:10:02 -0500

    Create TriviaScript.sh

---
+++ TriviaScript.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+echo 'Hello & Welcome to AWS Jeporady!'
+sleep 1
+echo 'Before we get started, What is the contestants name?!'
+read name
+echo ' Welcome to the show $name, Now lets get started!'
